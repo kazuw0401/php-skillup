b90fcc0b48a099e317aaba45f6e7464ef0c76f23
kazuw0401 <kazumasaw0401@gmail.com>
2019-10-11 20:31:37 +0900

    first commit

---
+++ app/checkbox_send.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>チェックボックスを使ったフォーム</title>
+</head>
+
+<body>
+  <h1>チェックボックスを使ったフォーム</h1>
+  <p>好きな色を選択してください（複数選択可）</p>
+  <p><input type="checkbox" name="colors[]" value="青">青
+    <input type="checkbox" name="colors[]" value="赤">赤
+    <input type="checkbox" name="colors[]" value="黄">黄
+    <input type="checkbox" name="colors[]" value="緑">緑
+    <input type="checkbox" name="colors[]" value="紫">紫
+    <input type="checkbox" name="colors[]" value="白">白
+    <input type="checkbox" name="colors[]" value="橙">橙
+  </p>
+  <input type="submit">
+</body>
+
+</html>
