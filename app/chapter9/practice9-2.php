34b7347192cb5ee796ff6b43dd482d6213a47727
kazuw0401 <kazumasaw0401@gmail.com>
2019-10-22 15:15:37 +0900

    chapter9-2終了

---
+++ app/chapter9/practice9-2.php
@@ -0,0 +1,10 @@
+<?php
+
+$str1 = '今日はくもりです。';
+$str2 = '今日は快晴でしょう。';
+
+$result1 = preg_match('/でしょう。$/u', $str1);
+$result2 = preg_match('/でしょう。$/u', $str2);
+
+var_dump($result1);
+var_dump($result2);
